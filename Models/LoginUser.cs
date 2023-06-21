127dd0a59cf0b51b4b051bcd17147658e81c0054
Benjamin <bjmcbride2@gmail.com>
2023-06-21 10:00:07 -0700

    edit

--- Models/LoginUser.cs
+++ Models/LoginUser.cs
@@ -1,5 +1,7 @@
 #pragma warning disable CS8618
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+[NotMapped]
 public class LoginUser
 {
   [Required]
